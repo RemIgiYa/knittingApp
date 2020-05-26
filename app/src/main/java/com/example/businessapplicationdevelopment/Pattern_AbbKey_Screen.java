6d5e3175f2fa382f5ed177c369f05e9697e63d6d
Ioana <ioanaavram46@yahoo.com>
2020-05-26 18:27:32 +0100

    test for  pattern key page

--- app/src/main/java/com/example/businessapplicationdevelopment/Pattern_AbbKey_Screen.java
+++ app/src/main/java/com/example/businessapplicationdevelopment/Pattern_AbbKey_Screen.java
@@ -3,12 +3,25 @@ package com.example.businessapplicationdevelopment;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class Pattern_AbbKey_Screen extends AppCompatActivity {
+
+    private TextView mKnitTest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pattern__abb_key__screen);
+        mKnitTest = (TextView) findViewById(R.id.knitTest);
+
+
+    }
+
+    public void Knits(View view) {
+        mKnitTest.setText(R.string.this_knit);
     }
 }
