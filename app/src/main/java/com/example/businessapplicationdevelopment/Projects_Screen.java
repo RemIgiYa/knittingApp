004fdacb384c30efc43ea0035482ac31d38b136a
Ioana <ioanaavram46@yahoo.com>
2020-06-25 19:14:05 +0100

    rest of the counters

--- app/src/main/java/com/example/businessapplicationdevelopment/Projects_Screen.java
+++ app/src/main/java/com/example/businessapplicationdevelopment/Projects_Screen.java
@@ -20,7 +20,14 @@ import java.util.ArrayList;
 public class Projects_Screen extends AppCompatActivity {
     private ListView mNotes;
     private int mCount;
+    private int mCount2;
+    private int mCount3;
+    private int mCount4;
     private TextView mRowCounter;
+    private TextView mRowCounter2;
+    private TextView mRowCounter3;
+    private TextView mRowCounter4;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +35,11 @@ public class Projects_Screen extends AppCompatActivity {
         setContentView(R.layout.activity_projects__screen);
         mNotes = (ListView) findViewById(R.id.noteView);
         mRowCounter = (TextView) findViewById(R.id.rowCounter1);
+        mRowCounter2 = (TextView) findViewById(R.id.rowCounter2);
+        mRowCounter3 = (TextView) findViewById(R.id.rowCounter3);
+        mRowCounter4 = (TextView) findViewById(R.id.rowCounter4);
+
+
     }
 
     @Override
@@ -74,10 +86,35 @@ public class Projects_Screen extends AppCompatActivity {
 
     }
 
-    public void countUp(View view) {
+    public void count1(View view) {
         mCount++;
         if (mRowCounter != null) {
             mRowCounter.setText(Integer.toString(mCount));
+        }
+
+
+    }
+
+    public void count2(View view) {
+        mCount2++;
+        if (mRowCounter2 != null) {
+            mRowCounter2.setText(Integer.toString(mCount2));
+        }
+
+    }
+
+    public void count3(View view) {
+        mCount3++;
+        if (mRowCounter3 != null) {
+            mRowCounter3.setText(Integer.toString(mCount3));
+        }
+
+    }
+
+    public void count4(View view) {
+        mCount4++;
+        if (mRowCounter4 != null) {
+            mRowCounter4.setText(Integer.toString(mCount4));
         }
     }
 
